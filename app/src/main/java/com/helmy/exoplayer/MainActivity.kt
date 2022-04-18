d1f8a7a47c7b68f2c3da9193f53f44b5090423ca
androTech0 <khbu2017@gmail.com>
2022-04-18 17:28:29 +0300

    test

--- app/src/main/java/com/helmy/exoplayer/MainActivity.kt
+++ app/src/main/java/com/helmy/exoplayer/MainActivity.kt
@@ -1,55 +1,105 @@
 package com.helmy.exoplayer
 
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
-import com.google.android.exoplayer2.extractor.ExtractorsFactory
-import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.source.MediaSource
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.trackselection.TrackSelector
-import com.google.android.exoplayer2.ui.SimpleExoPlayerView
-import com.google.android.exoplayer2.upstream.BandwidthMeter
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.*
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.util.Util
 
-class MainActivity : AppCompatActivity() {
 
-    lateinit var exoPlayerView: SimpleExoPlayerView
+class MainActivity : AppCompatActivity() , Player.Listener{
+
 
     // creating a variable for exoplayer
-     lateinit var exoPlayer: SimpleExoPlayer
 
     // url of video which we are loading.
+    private var mPlayer: SimpleExoPlayer? = null
+    private lateinit var playerView: PlayerView
+
     private var videoURL ="https://firebasestorage.googleapis.com/v0/b/courses-app-5c3b2.appspot.com/o/Videos%2FScreenrecorder-2022-04-18-06-06-09-890.mp4?alt=media&token=25939530-aa0d-412b-85f2-e038e5a6538b"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        exoPlayerView = findViewById(R.id.idExoPlayerVIew)
-        try {
-            val bandwidthMeter: BandwidthMeter =  DefaultBandwidthMeter()
-            val trackSelector: TrackSelector =  DefaultTrackSelector( AdaptiveTrackSelection.Factory(bandwidthMeter));
-            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
-            val videouri: Uri = Uri.parse(videoURL)
-            val dataSourceFactory =  DefaultHttpDataSourceFactory("exoplayer_video")
-            val extractorsFactory: ExtractorsFactory =  DefaultExtractorsFactory()
-            val mediaSource: MediaSource =  ExtractorMediaSource(videouri, dataSourceFactory, extractorsFactory, null, null);
-            exoPlayerView.setPlayer(exoPlayer)
-            exoPlayer.prepare(mediaSource)
-            exoPlayer.setPlayWhenReady(true)
+        playerView = findViewById(R.id.playerView)
 
-        } catch (e:Exception) {
-            // below line is used for
-            // handling our errors.
-            Log.e("TAG", "Error : $e")
+    }
+    private fun initPlayer() {
+
+        // Create a player instance.
+        mPlayer = SimpleExoPlayer.Builder(this).build()
+
+        // Bind the player to the view.
+        playerView.player = mPlayer
+
+        //setting exoplayer when it is ready.
+        mPlayer!!.playWhenReady = true
+
+        // Set the media source to be played.
+        mPlayer!!.setMediaSource(buildMediaSource())
+
+        // Prepare the player.
+        mPlayer!!.prepare()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) {
+            initPlayer()
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        if (Util.SDK_INT < 24 || mPlayer == null) {
+            initPlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
+    }
+
+
+    private fun releasePlayer() {
+        if (mPlayer == null) {
+            return
+        }
+        //release player when done
+        mPlayer!!.release()
+        mPlayer = null
+    }
+
+    //creating mediaSource
+    private fun buildMediaSource(): MediaSource {
+        // Create a data source factory.
+        val dataSourceFactory: DataSource.Factory = DefaultHttpDataSource.Factory()
+
+        // Create a progressive media source pointing to a stream uri.
+
+
+
+        val mediaSource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(MediaItem.fromUri(videoURL))
+
+        return mediaSource
+    }
 }
